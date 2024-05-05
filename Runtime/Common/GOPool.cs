e821e9cebf73846fb826ffaf8bfb21798de086ab
ZaroDev <victorfalcon796@gmail.com>
2024-05-05 13:08:40 +0200

    GO pool and meta files

---
+++ Runtime/Common/GOPool.cs
@@ -0,0 +1,116 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace ZaroDev.Utils.Runtime.Common
+{
+    public interface IPool<T>
+    {
+        T Pull();
+        void Push(T t);
+    }
+
+    public interface IPoolable<T>
+    {
+        void Initialize(System.Action<T> returnAction);
+        void ReturnToPool();
+    }
+
+    public class GOPool<T> : IPool<T> where T : MonoBehaviour, IPoolable<T>
+    {
+        public GOPool(GameObject pooledObject, int numToSpawn = 0)
+        {
+            this.prefab = pooledObject;
+            Spawn(numToSpawn);
+        }
+
+        public GOPool(GameObject pooledObject, Action<T> pullObject, Action<T> pushObject, int numToSpawn = 0)
+        {
+            this.prefab = pooledObject;
+            this.pullObject = pullObject;
+            this.pushObject = pushObject;
+            Spawn(numToSpawn);
+        }
+
+        private System.Action<T> pullObject;
+        private System.Action<T> pushObject;
+        private Stack<T> pooledObjects = new Stack<T>();
+        private GameObject prefab;
+
+        public int pooledCount
+        {
+            get { return pooledObjects.Count; }
+        }
+
+        public T Pull()
+        {
+            T t;
+            t = pooledCount > 0 ? pooledObjects.Pop() : GameObject.Instantiate(prefab).GetComponent<T>();
+
+            t.gameObject.SetActive(true); //ensure the object is on
+            t.Initialize(Push);
+
+            //allow default behavior and turning object back on
+            pullObject?.Invoke(t);
+
+            return t;
+        }
+
+        public T Pull(Vector3 position)
+        {
+            T t = Pull();
+            t.transform.position = position;
+            return t;
+        }
+
+        public T Pull(Vector3 position, Quaternion rotation)
+        {
+            T t = Pull();
+            t.transform.position = position;
+            t.transform.rotation = rotation;
+            return t;
+        }
+
+        public GameObject PullGameObject()
+        {
+            return Pull().gameObject;
+        }
+
+        public GameObject PullGameObject(Vector3 position)
+        {
+            GameObject go = Pull().gameObject;
+            go.transform.position = position;
+            return go;
+        }
+
+        public GameObject PullGameObject(Vector3 position, Quaternion rotation)
+        {
+            GameObject go = Pull().gameObject;
+            go.transform.position = position;
+            go.transform.rotation = rotation;
+            return go;
+        }
+
+        public void Push(T t)
+        {
+            pooledObjects.Push(t);
+
+            //create default behavior to turn off objects
+            pushObject?.Invoke(t);
+
+            t.gameObject.SetActive(false);
+        }
+
+        private void Spawn(int number)
+        {
+            T t;
+
+            for (int i = 0; i < number; i++)
+            {
+                t = GameObject.Instantiate(prefab).GetComponent<T>();
+                pooledObjects.Push(t);
+                t.gameObject.SetActive(false);
+            }
+        }
+    }
+}
